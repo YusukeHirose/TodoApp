a9f8d45c157b86fba4640af13ec8dc2a41950bc4
茅島 佑樹 <kayashimayuki@LAB-N1319.local>
2018-03-24 15:46:43 +0900

    create modl Todo.java

---
+++ app/models/Todo.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import javax.validation.*;
+import play.db.ebean.*;
+import play.data.validation.*;
+
+@Entity
+public class Todo extends Model {
+
+  @Id
+  public Long id;
+
+  @Constraints.Required
+  @Constraints.Min(1)
+  @Constraints.Max(31)
+  public String title;
+
+  @Constraints.Required
+  public Date deadline;
+
+  public Date createdAt;
+
+  public Date updatedAt;
+
+}
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,25 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table todo (
+  id                        bigint auto_increment not null,
+  title                     varchar(255),
+  deadline                  datetime,
+  created_at                datetime,
+  updated_at                datetime,
+  constraint pk_todo primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table todo;
+
+SET FOREIGN_KEY_CHECKS=1;
+
