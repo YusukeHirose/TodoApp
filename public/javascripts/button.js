9bd1c7bfb3550f29400d3d62ab887753c6fd9b54
茅島 佑樹 <kayashimayuki@LAB-N1319.local>
2018-03-28 12:58:26 +0900

    modify button.js

--- public/javascripts/button.js
+++ public/javascripts/button.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('#state_button').on('click', function () {
+    $(document).on('click', '#state_button', function () {
         var target = $(this).val();
         if (target === '未完了') {
             $(this).val('完了');
