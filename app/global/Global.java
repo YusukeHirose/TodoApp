7d18eca85c12be9e4ea933ce1e8042a562444f3e
茅島 佑樹 <kayashimayuki@LAB-N1319.local>
2018-04-10 09:46:57 +0900

    add onHandlerNotFound method to Gloabal.java

--- app/global/Global.java
+++ app/global/Global.java
@@ -9,7 +9,11 @@ import static play.mvc.Results.*;
 public class Global extends GlobalSettings {
     
     public Promise<SimpleResult> onError(RequestHeader request, Throwable t) {
-        return Promise.<SimpleResult>pure(internalServerError(views.html.index.render(t.toString(), models.Todo.all(), controllers.Application.todoForm)));
+        return Promise.<SimpleResult>pure(internalServerError(views.html.error.render("ページを表示できません。")));
+    }
+
+    public Promise<SimpleResult> onHandlerNotFound(RequestHeader request) {
+        return Promise.<SimpleResult>pure(notFound(views.html.error.render("ページが見つかりません。")));
     }
 
 }
